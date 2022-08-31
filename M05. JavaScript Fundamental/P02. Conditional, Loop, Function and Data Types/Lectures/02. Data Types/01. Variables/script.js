c29a05be93857712962a13e49722a8ce45589627
satoshiuy <lequocuyit@gmail.com>
2022-08-31 10:13:35 +0700

    rename file

---
+++ M05. JavaScript Fundamental/P02. Conditional, Loop, Function and Data Types/Lectures/02. Data Types/01. Variables/script.js
@@ -0,0 +1,24 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+
+var msg; 
+msg = "Hello JavaScript!"; // assigned a string value
+alert(msg); // access a variable
+
+//the following declares and assign a numeric value
+var num = 100; 
+var hundred = num;  // assigned a variable to varible
+
+
+// ===============================
+
+var myvariable = 1;  // numeric value
+
+myvariable = 'one'; // string value
+
+myvariable = 1.1; // decimal value
+
+myvariable = true; // Boolean value
+
+myvariable = null; // null value
---
+++ M05. JavaScript Fundamental/P02. Conditional, Loop, Function and Data Types/Lectures/02. Data Types/02. Strings/script.js
@@ -0,0 +1,23 @@
+const text1 = 'hello';
+const text2 = 'world';
+const text3 = '     JavaScript    ';
+
+// concatenating two strings
+const result1 = text1.concat(' ', text2);
+console.log(result1); // "hello world"
+
+// converting the text to uppercase
+const result2 = text1.toUpperCase();
+console.log(result2); // HELLO
+
+// removing whitespace from the string
+const result3 = text3.trim();
+console.log(result3); // JavaScript
+
+// converting the string to an array
+const result4 = text1.split();
+console.log(result4); // ["hello"]
+
+// slicing the string
+const result5= text1.slice(1, 3);
+console.log(result5); // "el"
